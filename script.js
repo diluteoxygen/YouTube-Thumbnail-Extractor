b9c8798797ecd0bb826ee9093378a050e5f33677
Dilute Oxygen <vikrantsinghtomar011@gmail.com>
2022-12-20 21:10:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,27 @@ const extractButton = document.getElementById('extract-button');
 const thumbnailContainer = document.getElementById('thumbnail-container');
 
 extractButton.addEventListener('click', () => {
-  // Remove any existing thumbnail image
+  // Remove any existing thumbnail image and download button
   thumbnailContainer.innerHTML = '';
 
-  // Extract the thumbnail image from the new URL
+  // Extract the thumbnail image from the URL
   const url = document.getElementById('url').value;
   const videoId = getVideoId(url);
   const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
   const thumbnailImg = document.createElement('img');
   thumbnailImg.src = thumbnailUrl;
   thumbnailContainer.appendChild(thumbnailImg);
+
+  // Create a download button and append it to the thumbnail container
+  const downloadButton = document.createElement('button');
+  downloadButton.innerHTML = 'Download';
+  downloadButton.addEventListener('click', () => {
+    const link = document.createElement('a');
+    link.href = thumbnailUrl;
+    link.download = 'thumbnail.jpg';
+    link.click();
+  });
+  thumbnailContainer.appendChild(downloadButton);
 });
 
 function getVideoId(url) {
